9efcc1735c1ab11028739ad2ab741e9074f12caa
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-09-02 15:59:27 +0300

    added Quiz creaator bl

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,168 @@
 import React from 'react';
+import s from './QuizCreator.module.css';
+import Button from '../../components/UI/Button/Button';
+import { createControl, validate, validateForm } from '../../form/formFramework';
+import Input from '../../components/UI/Input/Input';
+import Select from '../../components/UI/Select/Select';
 
+// локальные функции полезные только для этого модуля
+function createOptionControl(number) {
+  return createControl({
+    label: `Вариант ответа № ${number}`,
+    errorMessage: 'Поле не может быть пустым',
+    id: number
+  },
+    { required: true }
+  )
+}
+
+function createFormControls() {
+  return {
+    question: createControl({
+      label: 'Введите вопрос',
+      errorMessage: 'Поле не может быть пустым'
+    }, { required: true }),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4),
+  }
+}
+// сам объект
 export default class QuizCreator extends React.Component {
+
+  state = {
+    quiz: [],
+    isFormValid: false,
+    rightAnswerId: 1,
+    formControls: createFormControls(),
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  addQuestionHandler = (e) => {
+    e.preventDefault();
+
+    const quiz = this.state.quiz.concat();
+    const index = quiz.length + 1;
+
+    const { question, option1, option2, option3, option4 } = this.state.formControls;
+
+    const questionItem = {
+      question: this.state.formControls.question.value,
+      id: index,
+      rightAnswerId: this.state.rightAnswerId,
+      answers: [
+        { text: option1.value, id: option1.id },
+        { text: option2.value, id: option2.id },
+        { text: option3.value, id: option3.id },
+        { text: option4.value, id: option4.id },
+      ]
+    }
+
+    quiz.push(questionItem);
+
+    this.setState({
+      quiz: quiz,
+      isFormValid: false,
+      rightAnswerId: 1,
+      formControls: createFormControls(),
+    })
+  }
+
+  createQuizHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state.quiz);
+
+    //TODO: server
+  }
+
+  changeHandler = (value, controlName) => {
+    //создаем копию стейта
+    const formControls = { ...this.state.formControls }
+    //создаем переменную с текущим контролом
+    const control = { ...formControls[controlName] }
+
+    control.touched = true;
+    control.value = value;
+    control.valid = validate(control.value, control.validation);
+
+    formControls[controlName] = control;
+
+    this.setState({
+      formControls: formControls,
+      isFormValid: validateForm(formControls)
+    })
+  }
+
+  renderControls() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+
+      return (
+        <React.Fragment key={index}>
+          <Input
+
+            label={control.label}
+            type={control.type}
+            value={control.value}
+            valid={control.valid}
+            shouldValidate={!!control.validation}
+            touched={control.touched}
+            errorMessage={control.errorMessage}
+            onChange={e => this.changeHandler(e.target.value, controlName)}
+          />
+          {index === 0 ? <hr /> : null}
+        </React.Fragment>
+      )
+    })
+  }
+
+  selectChangeHandler = (e) => {
+    this.setState({
+      rightAnswerId: +e.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={s.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+          <form onSubmit={this.submitHandler}>
+
+            {this.renderControls()}
+
+            <Select
+              label="Выберите правильный ответ"
+              value={this.state.rightAnswerId}
+              onChange={this.selectChangeHandler}
+              options={[
+                { text: 1, value: 1 },
+                { text: 2, value: 2 },
+                { text: 3, value: 3 },
+                { text: 4, value: 4 },
+              ]}
+            ></Select>
+            <Button
+              type='primary'
+              onClick={this.addQuestionHandler}
+              disabled={!this.state.isFormValid}
+            >
+              Добавить вопрос
+            </Button>
+
+            <Button
+              type='success'
+              onClick={this.createQuizHandler}
+              disabled={this.state.quiz.length === 0}
+            >
+              Создать тест
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
---
+++ src/form/formFramework.js
@@ -0,0 +1,36 @@
+// принимает конфигурацию и набор правил валидации и возарвщает объект с правилами
+export function createControl(config, validation) {
+  return {
+    ...config,
+    validation,
+    valid: !validation,
+    touched: false,
+    value: ''
+  }
+};
+
+export function validate(value, validation = null) {
+  if (!validation) {
+    return true;
+  }
+
+  let isValid = true;
+
+  if (validation.required) {
+    isValid = value.trim() !== '' && isValid
+  }
+
+  return isValid;
+};
+
+export function validateForm(formControls) {
+  let isFormValid = true;
+
+  for (let control in formControls) {
+    if (formControls.hasOwnProperty(control)) {
+      isFormValid = formControls[control].valid && isFormValid;
+    }
+  }
+
+  return isFormValid
+}
